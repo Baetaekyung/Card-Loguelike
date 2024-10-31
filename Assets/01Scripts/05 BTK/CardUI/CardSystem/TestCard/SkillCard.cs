791319b61fb5e519024e064abeae1e085a9bb1d0
Baetaekyung <tkwayi2129@gmail.com>
2024-11-01 00:05:19 +0900

    Test

--- Assets/01Scripts/05 BTK/CardUI/CardSystem/TestCard/SkillCard.cs
+++ Assets/01Scripts/05 BTK/CardUI/CardSystem/TestCard/SkillCard.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class SkillCard : BaseCard
 {
+    [SerializeField] private ParticleSystem _skillTypeEffect;
+
     protected virtual void OnEnable()
     {
         OnCardUseEvent += SkillExcute;
@@ -10,6 +12,7 @@ public class SkillCard : BaseCard
     protected virtual void SkillExcute()
     {
         Debug.Log("스킬 사용");
+        _skillTypeEffect.Play();
     }
 
     protected virtual void OnDestroy()
