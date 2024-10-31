59fe290ba503a4ae052e1bbfb66394012534985a
sinsanghyeon <sinsanghyeon1234@gmail.com>
2024-10-31 23:25:15 +0900

    기능구현 : 에디터로 SO 뽑아내기

---
+++ Assets/01Scripts/02 SSH/Editor/CardSOCustomEditor.cs
@@ -0,0 +1,64 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+public class CardSOCustomEditor : EditorWindow
+{
+    private CardDataSO _dataSO;
+
+    public CardType cardType;
+    public CardInfo cardInfo;
+
+    [MenuItem("Tools/Utility")]
+    public static void ShowWindow()
+    {
+        GetWindow<CardSOCustomEditor>("test");
+    }
+
+    private void OnEnable()
+    {
+        _dataSO = CreateInstance<CardDataSO>(); // _dataSO 초기화
+    }
+
+    private void OnGUI()
+    {
+        // CardInfo의 필드 입력
+        cardInfo.cardName = EditorGUILayout.TextField("Name", cardInfo.cardName);
+        cardInfo.cardDescription = EditorGUILayout.TextField("Description", cardInfo.cardDescription);
+        // cardInfo.cardEffectDescription = EditorGUILayout.TextField("EffectDescription", cardInfo.cardEffectDescription);
+        cardInfo.cost = EditorGUILayout.IntField("Cost", cardInfo.cost);
+        cardInfo.cardSprite = (Sprite)EditorGUILayout.ObjectField("sprite", cardInfo.cardSprite, typeof(Sprite), false);
+        cardInfo.cardRarity = (CardRarity)EditorGUILayout.EnumPopup("Rarity", cardInfo.cardRarity);
+        
+        
+        // CardType 입력
+        cardType = (CardType)EditorGUILayout.EnumPopup("Type", cardType);
+
+        if (GUILayout.Button("Create Scriptable Object"))
+        {
+            CreateScriptableObject();
+        }
+    }
+
+    private void CreateScriptableObject()
+    {
+        // 새로운 ScriptableObject 인스턴스 생성
+        var asset = CreateInstance<CardDataSO>();
+        asset.cardInfo = cardInfo;
+        asset.cardType = cardType;
+
+        // 파일 경로 생성
+        string path = AssetDatabase.GenerateUniqueAssetPath($"Assets/00SODatas/02 SSH/Created/{cardInfo.cardName}.asset");
+
+        // ScriptableObject 저장
+        AssetDatabase.CreateAsset(asset, path);
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+
+        // 새로 생성된 ScriptableObject를 선택하고 포커스
+        Selection.activeObject = asset;
+        EditorUtility.FocusProjectWindow();
+
+        Debug.Log($"Created ScriptableObject at: {path}");
+    }
+}
