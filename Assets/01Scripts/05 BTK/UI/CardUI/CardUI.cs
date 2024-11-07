f4025652acd514a1b1e1279734882ed08f30ef34
Baetaekyung <tkwayi2129@gmail.com>
2024-11-07 09:52:10 +0900

    Title scene

---
+++ Assets/01Scripts/05 BTK/UI/CardUI/CardUI.cs
@@ -0,0 +1,32 @@
+public class CardUI : BaseCard
+{
+    public bool isOnDeck = false;
+
+    private void OnEnable()
+    {
+        OnPointerDownEvent += OnPointerDownHandler;
+        isOnDeck = false;
+    }
+
+    private void OnPointerDownHandler()
+    {
+        CardManager cardManager = CardManager.Instance;
+
+        isOnDeck = !isOnDeck;
+
+        if(isOnDeck)
+            cardManager.deckCnt++;
+        else
+            cardManager.deckCnt--;
+    }
+
+    protected override void UpdatePosition()
+    {
+        //dont update position
+    }
+
+    private void OnDestroy()
+    {
+        OnPointerDownEvent -= OnPointerDownHandler;
+    }
+}
