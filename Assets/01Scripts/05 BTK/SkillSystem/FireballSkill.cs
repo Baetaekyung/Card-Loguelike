8d13523a017b467b8d77163f8fc7925401a3e53e
sinsanghyeon <sinsanghyeon1234@gmail.com>
2024-11-12 09:12:30 +0900

    Created Fireball

--- Assets/01Scripts/05 BTK/SkillSystem/FireballSkill.cs
+++ Assets/01Scripts/05 BTK/SkillSystem/FireballSkill.cs
@@ -1,11 +1,21 @@
+using System;
 using CardGame.Players;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace CardGame
 {
     public class FireballSkill : BaseSkill
     {
-        public override void UseSkill(Player owner)
+        
+
+       [SerializeField]private ActionData _data;
+
+       private void Start()
+       {
+       }
+
+       public override void UseSkill(Player owner)
         {
             
         }
